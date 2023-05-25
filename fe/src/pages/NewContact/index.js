0bb256b736ed1a50c0173ce2df785f7da569549c
Junior Cundari <jrcundari@yahoo.com.br>
2023-05-25 10:27:06 -0300

    Criacao do ContactMapper

--- fe/src/pages/NewContact/index.js
+++ fe/src/pages/NewContact/index.js
@@ -1,21 +1,17 @@
 import { useRef } from 'react';
+
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
+
 import ContactsService from '../../services/ContactsService';
+
 import toast from '../../utils/toast';
 
 export default function NewContact() {
   const contactFormRef = useRef(null);
 
-  async function handleSubmit(formData) {
+  async function handleSubmit(contact) {
     try {
-      const contact = {
-        name: formData.name,
-        email: formData.email,
-        phone: formData.phone,
-        category_id: formData.categoryId,
-      };
-
       await ContactsService.createContact(contact);
 
       contactFormRef.current.resetFields();
