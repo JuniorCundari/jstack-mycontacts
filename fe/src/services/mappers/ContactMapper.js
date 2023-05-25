aca21de37ba44662a1aded0c378e63e8b725bd3b
Junior Cundari <jrcundari@yahoo.com.br>
2023-05-25 11:02:22 -0300

    Implementado o toDomain no ContactMapper

--- fe/src/services/mappers/ContactMapper.js
+++ fe/src/services/mappers/ContactMapper.js
@@ -8,9 +8,18 @@ class ContactMapper {
     };
   }
 
-  // toDomain(persistenceContact) {
-  //   return {};
-  // }
+  toDomain(persistenceContact) {
+    return {
+      id: persistenceContact.id,
+      name: persistenceContact.name,
+      email: persistenceContact.email,
+      phone: persistenceContact.phone,
+      category: {
+        id: persistenceContact.category_id,
+        name: persistenceContact.category_name,
+      },
+    };
+  }
 }
 
 export default new ContactMapper();
